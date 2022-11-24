fa240c0247aa630cee4825973a220ca2e7852e4c
Shourov Hasan <shourovhasan172@gmail.com>
2022-11-25 00:50:27 +0600

    basic server created and run

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/',(req, res) =>{
+  res.send('Camera Corner Server is Running.......');
+});
+
+
+app.listen(port, () =>{
+  console.log(`Camera Corner Server running on ${port} port`);
+});
