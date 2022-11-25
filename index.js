f2c194c040c2cc30b8ad050f5d027dc3ffed6677
Shourov Hasan <shourovhasan172@gmail.com>
2022-11-25 09:21:11 +0600

    connect json web token

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
+require('dotenv').config();
 
 
 
@@ -17,14 +19,37 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 
 
+// Jwt Token Area
+function verifyJWT(req, res, next){
+
+  const authHeader = req.headers.authorization;
+
+  if(!authHeader){
+      return res.status(401).send({message: 'unauthorized access'});
+  }
+  const token = authHeader.split(' ')[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded){
+      if(err){
+          return res.status(403).send({message: 'Forbidden access'});
+      }
+      req.decoded = decoded;
+      next();
+  })
+}
 
 
 
+
+
+
+//basic data get
 app.get('/',(req, res) =>{
   res.send('Camera Corner Server is Running.......');
 });
 
 
+//port running console
 app.listen(port, () =>{
   console.log(`Camera Corner Server running on ${port} port`);
 });
